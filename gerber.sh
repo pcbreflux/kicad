9a85a2ac9fb6deef877975c1a0f9e4deae91dc80
pcbreflux <pcbreflux@gmail.com>
2017-09-12 15:14:32 +0200

    KiCad to gerber

--- gerber.sh
+++ gerber.sh
@@ -1,9 +1,12 @@
-mv $1.drl $1.txt
-mv $1-B.Cu.gbr $1.GBL
-mv $1-B.Mask.gbr $1.GBS
-mv $1-B.SilkS.gbr $1.GBO
-mv $1-Edge.Cuts.gbr $1.GML
-mv $1-F.Cu.gbr $1.GTL
-mv $1-F.Mask.gbr $1.GTS
-mv $1-F.SilkS.gbr $1.GTO
-mv $1-Margin.gbr $1.GBR
+mv "$1.drl" "$1.txt"
+mv "$1-B.Cu.gbr" "$1.GBL"
+mv "$1-B.Mask.gbr" "$1.GBS"
+mv "$1-B.SilkS.gbr" "$1.GBO"
+mv "$1-Edge.Cuts.gbr" "$1.GML"
+mv "$1-F.Cu.gbr" "$1.GTL"
+mv "$1-F.Mask.gbr" "$1.GTS"
+mv "$1-F.SilkS.gbr" "$1.GTO"
+mv "$1-Margin.gbr" "$1.GBR"
+mv "$1-B.Paste.gbr" "$1.GBP"
+mv "$1-F.Paste.gbr" "$1.GTP"
+
